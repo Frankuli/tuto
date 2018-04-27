5ae268cf7b1a36fac9b56b1142a834038c5240cc
Franco <f.manassero@itecriocuarto.org.ar>
2018-04-26 22:56:56 -0300

    mela

--- sinatra.rb
+++ sinatra.rb
@@ -1,17 +1,1 @@
-require 'sinatra'
-require 'sinatra/reloader'
-
-get '/saludo/martin' do
-	"Hola Mundo"
-end
-#ejecucion normal
-
-get '/saludo/:nombre/:apellido' do
-	"Hola #{params[:nombre]} #{params[:apellido]}"
-end
-
-get '/saludo_con_formato/:nombre' do
-	@nombre = params[:nombre]
-	erb :saludo
-end
-
+la cage
